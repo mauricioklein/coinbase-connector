63be54851208a3ca2714f8b7b4e664b74c8121d0
Mauricio Klein <mauricio.klein@blacklane.com>
2018-12-08 16:16:14 +0100

    Add signer module
    This module is responsible to generate the Coinbase signature, provide in
    all private API calls

---
+++ signer.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+)
+
+func generateSignature(body, timestamp, secret string) (string, error) {
+	preHash := fmt.Sprintf("%s%s%s%s", timestamp, "POST", "/orders", body)
+
+	decodedSecret, err := base64.StdEncoding.DecodeString(secret)
+	if err != nil {
+		return "", err
+	}
+
+	signature := hmac.New(sha256.New, decodedSecret)
+
+	if _, err = signature.Write([]byte(preHash)); err != nil {
+		return "", err
+	}
+
+	return base64.StdEncoding.EncodeToString(signature.Sum(nil)), nil
+}
