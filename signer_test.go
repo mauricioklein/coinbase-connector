5ba2b6a1d3d62187813e5bdf46966b02d770314c
Mauricio Klein <mauricio.klein@blacklane.com>
2018-12-09 22:51:23 +0100

    Add edge case test for signer

--- signer_test.go
+++ signer_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGenerateSignature(t *testing.T) {
+func TestGenerateSignature_Success(t *testing.T) {
 	body := `{"foo": "bar"}`
 	timestamp := "1544281725.56"
 	secret := "ABCD1234"
@@ -15,4 +15,15 @@ func TestGenerateSignature(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, signature, "kqKSisBO0iUFuyPdbCgsnRelHo/A3LbxsOqE02/v3Hk=")
+}
+
+func TestGenerateSignature_InvalidSecret(t *testing.T) {
+	body := `{"foo": "bar"}`
+	timestamp := "1544281725.56"
+	secret := "!#$"
+
+	signature, err := generateSignature(body, timestamp, secret)
+
+	assert.Error(t, err)
+	assert.Equal(t, signature, "")
 }
